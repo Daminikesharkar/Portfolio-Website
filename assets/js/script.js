c1d9422f28d3153ba78f014ee5bd534a819c6285
Damini Kesharkar <daminisk272@gmail.com>
2024-01-27 00:31:56 +0530

    Created a portfolio website, added nav header and home section into the wedsite using html css and js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".variabletext", {
+    strings: ["Backend development","Frontend development", "Fullstack development"],
+    loop: true,
+    backDelay: 300,
+    typeSpeed: 60,
+    backSpeed: 30,    
+});
